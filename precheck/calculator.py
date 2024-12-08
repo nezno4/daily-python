f30d475a2fd4e90a36b6bc3242f06da1fe788c56
Nez <zenitechq@gmail.com>
2024-12-08 23:31:40 +0100

    chore(hooks): check files by hooks

--- precheck/calculator.py
+++ precheck/calculator.py
@@ -1,14 +1,18 @@
+# -*- coding: utf-8 -*-
 def add(a: float, b: float) -> float:
     """Adds two numbers."""
     return a + b
+
 
 def subtract(a: float, b: float) -> float:
     """Subtracts the second number from the first."""
     return a - b
 
+
 def multiply(a: float, b: float) -> float:
     """Multiplies two numbers."""
     return a * b
+
 
 def divide(a: float, b: float) -> float:
     """Divides the first number by the second. Raises an exception when dividing by zero."""
