0d0122579a720c5f49947807eef51c136e628314
Nez <zenitechq@gmail.com>
2024-12-08 16:56:05 +0100

    chore(calculator.py, test_calculator.py): add simple class for test configuration

---
+++ precheck/calculator.py
@@ -0,0 +1,17 @@
+def add(a: float, b: float) -> float:
+    """Adds two numbers."""
+    return a + b
+
+def subtract(a: float, b: float) -> float:
+    """Subtracts the second number from the first."""
+    return a - b
+
+def multiply(a: float, b: float) -> float:
+    """Multiplies two numbers."""
+    return a * b
+
+def divide(a: float, b: float) -> float:
+    """Divides the first number by the second. Raises an exception when dividing by zero."""
+    if b == 0:
+        raise ValueError("Cannot divide by zero!")
+    return a / b
